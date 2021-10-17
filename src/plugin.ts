2a5055f12488dd3f141cfc60e5e98938200a95c8
Matt Hamlin <matthewjameshamlin@gmail.com>
2021-10-17 15:26:00 -0400

    Adding some types

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,16 +1,44 @@
+import {
+  ImportSpecifier,
+  ImportDefaultSpecifier,
+  ImportDeclaration,
+  CallExpression,
+} from '@babel/types'
+
+type AnyImportSpecifier = ImportDefaultSpecifier | ImportSpecifier
+
+interface ImportDeclarationPath {
+  node: ImportDeclaration
+}
+
+interface CallExpressionPath {
+  node: CallExpression
+}
+
 export default function StaticStyleSheetsPlugin(babel) {
   const { types: t } = babel
 
   return {
     name: 'static-style-sheets/plugin',
     visitor: {
-      Program() {},
+      Program() {
+        this.stylesImport = null
+      },
       // Handle imports...
       // ex:
       // ```tsx
       // import styles from 'static-style-sheets';
       // ```
-      ImportDeclaration(path) {},
+      ImportDeclaration(path: ImportDeclarationPath) {
+        if (path.node.source.value === 'static-style-sheets') {
+          this.stylesImport = {
+            importedAs: path.node.specifiers.find(
+              (spec: AnyImportSpecifier) =>
+                spec.type === 'ImportDefaultSpecifier',
+            ).local.name,
+          }
+        }
+      },
       // Handle variable declarations, assignment expressions, etc...
       // ex:
       // ```tsx
@@ -23,7 +51,7 @@ export default function StaticStyleSheetsPlugin(babel) {
       // // ...
       // let element = <button className={styles({color: 'purple'})}>Yo</button>
       // ```
-      CallExpression(path) {},
+      CallExpression(path: CallExpressionPath) {},
     },
   }
 }
